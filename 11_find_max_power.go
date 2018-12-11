9e0c436220b2d35095e4cf4291c02ab21be5da8b
Maxim Litvinov <metalim@gmail.com>
2018-12-11 09:12:18 +0200

    2018.11: use much faster Summed-area table

--- 11_find_max_power.go
+++ 11_find_max_power.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"time"
 )
 
@@ -15,34 +16,37 @@ func _log(a ...interface{}) {
 
 type prep [][]int
 
+//
+// Inspired by hint given by https://github.com/petertseng
+// Prepare Summed-area table, instead of just a power map.
+// https://en.wikipedia.org/wiki/Summed-area_table
+//
 func prepare(in int) prep {
-	m := make(prep, 0, 300)
-	for y := 1; y <= 300; y++ {
-		r := make([]int, 0, 300)
-		for x := 1; x <= 300; x++ {
+	m := make(prep, 0, 301)
+	for y := 0; y <= 300; y++ {
+		m = append(m, make([]int, 301))
+	}
+	for y := 1; y < 300; y++ {
+		for x := 1; x < 300; x++ {
 			rid := x + 10
 			p := (rid*y+in)*rid/100%10 - 5
-			r = append(r, p)
+			m[y][x] = p + m[y][x-1] + m[y-1][x] - m[y-1][x-1]
 		}
-		m = append(m, r)
 	}
 	return m
 }
 
-func find(m prep, d int) [4]int {
+func find(m prep, dMin, dMax int) [4]int {
 	maxp := 0
 	var out [4]int
-	for y := 0; y <= 300-d; y++ {
-		for x := 0; x <= 300-d; x++ {
-			p := 0
-			for dy := 0; dy < d; dy++ {
-				for dx := 0; dx < d; dx++ {
-					p += m[y+dy][x+dx]
+	for d := dMin; d <= dMax; d++ {
+		for y := 1; y < 300-d; y++ {
+			for x := 1; x < 300-d; x++ {
+				p := m[y+d-1][x+d-1] - m[y+d-1][x-1] - m[y-1][x+d-1] + m[y-1][x-1]
+				if maxp < p {
+					maxp = p
+					out = [4]int{x, y, d, p}
 				}
-			}
-			if maxp < p {
-				maxp = p
-				out = [4]int{x, y, d, p}
 			}
 		}
 	}
@@ -50,69 +54,39 @@ func find(m prep, d int) [4]int {
 }
 
 func part1(m prep) string {
-	p := find(m, 3)
-	return fmt.Sprintf("%d,%d power %d", p[0]+1, p[1]+1, p[3])
-}
-
-func (sums *prep) add(m prep, d int) [4]int {
-	maxp := 0
-	var out [4]int
-	for y := 0; y <= 300-d; y++ {
-		for x := 0; x <= 300-d; x++ {
-			p := 0
-			for dy := 0; dy < d; dy++ {
-				p += m[y+dy][x+d-1]
-			}
-			for dx := 0; dx < d-1; dx++ {
-				p += m[y+d-1][x+dx]
-			}
-			p += (*sums)[y][x]
-			(*sums)[y][x] = p
-			if maxp < p {
-				maxp = p
-				out = [4]int{x, y, d, p}
-			}
-		}
-	}
-	return out
+	p := find(m, 3, 3)
+	return fmt.Sprintf("%d,%d power %d", p[0], p[1], p[3])
 }
 
 func part2(m prep) string {
-	out := find(m, 1)
-	sums := make(prep, 300)
-	for i := 0; i < 300; i++ {
-		sums[i] = make([]int, 300)
-		copy(sums[i], m[i])
-	}
-	for d := 2; d <= 300; d++ {
-		p := sums.add(m, d)
-		if out[3] < p[3] {
-			out = p
-		}
-	}
-	return fmt.Sprintf("%d,%d,%d power %d", out[0]+1, out[1]+1, out[2], out[3])
+	out := find(m, 1, 300)
+	return fmt.Sprintf("%d,%d,%d power %d", out[0], out[1], out[2], out[3])
 }
 
 //
 // tests
 //
 
-/*
-func verify(p prep, ex int) {
-	v := part1(p)
+func verify(v, ex string) {
 	if v != ex {
-		log.Fatal(v, "!=", ex)
+		log.Fatalf(`"%s" != "%s"`, v, ex)
 	}
 }
 
 func test() {
-	// verify(prep{}, 1)
+	p := prepare(18)
+	verify(part1(p), "33,45 power 29")
+	verify(part2(p), "90,269,16 power 113")
+
+	p = prepare(42)
+	verify(part1(p), "21,61 power 30")
+	verify(part2(p), "232,251,11 power 119")
+
 	fmt.Println("tests passed")
 }
-*/
 
 func main() {
-	// test()
+	test()
 	for i, in := range ins {
 		fmt.Println("=== for", i, "===")
 		t0 := time.Now()
