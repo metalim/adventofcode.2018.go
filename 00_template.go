63241ddecf3fd684f56ae2d494ea9f6c3a3e66f8
Maxim Litvinov <metalim@gmail.com>
2018-12-17 00:52:38 +0200

    update template

--- 00_template.go
+++ 00_template.go
@@ -6,10 +6,32 @@ import (
 	"strconv"
 	"strings"
 	"time"
+
+	. "github.com/logrusorgru/aurora"
 )
 
 func _log(a ...interface{}) {
 	fmt.Println(a...)
+}
+
+func _log1(a ...interface{}) {
+	fmt.Println(a...)
+	fmt.Scanln()
+}
+
+func sliceAtoi(in []string) []int {
+	out := make([]int, len(in))
+	for i, v := range in {
+		out[i], _ = strconv.Atoi(v)
+	}
+	return out
+}
+
+func abs(n int) int {
+	if n < 0 {
+		return -n
+	}
+	return n
 }
 
 //
@@ -21,19 +43,17 @@ type prep struct{}
 func prepare(in string) prep {
 	ss := strings.Split(in, "\n")
 	for _, s := range ss {
-		_log(s)
+		s = s
 	}
 	return prep{}
 }
 
 func part1(p prep) int {
-	n, _ := strconv.Atoi("1")
-	return n
+	return 1
 }
 
 func part2(p prep) int {
-	n, _ := strconv.Atoi("2")
-	return n
+	return 2
 }
 
 //
@@ -55,15 +75,24 @@ func test() {
 func main() {
 	test()
 	for i, in := range ins {
-		fmt.Println("=== for", i, "===")
-		t0 := time.Now()
+		fmt.Println(Brown(fmt.Sprint("=== for ", i, " ===")))
+		var t0, t1 time.Time
+
+		t0 = time.Now()
 		p := prepare(in)
-		fmt.Println("part 1:", part1(p))
-		t1 := time.Now()
-		fmt.Println(t1.Sub(t0))
-		fmt.Println("part 2:", part2(p))
-		t2 := time.Now()
-		fmt.Println(t2.Sub(t1))
+		t1 = time.Now()
+		fmt.Println(Gray("prepare:"), Black(t1.Sub(t0)).Bold())
+
+		t0 = time.Now()
+		v1 := part1(p)
+		t1 = time.Now()
+		fmt.Println(Gray("part 1:"), Black(t1.Sub(t0)).Bold(), Green(v1).Bold())
+
+		t0 = time.Now()
+		v2 := part2(p)
+		t1 = time.Now()
+		fmt.Println(Gray("part 2:"), Black(t1.Sub(t0)).Bold(), Green(v2).Bold())
+
 		fmt.Println()
 	}
 }
